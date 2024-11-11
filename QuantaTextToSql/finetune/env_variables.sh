51f97d8b2426f86fe720f04170a2a46dae02af45
Dhruv <dhruv@gretel.ai>
2024-11-11 19:37:27 +0000

    update env variables

--- QuantaTextToSql/finetune/env_variables.sh
+++ QuantaTextToSql/finetune/env_variables.sh
@@ -1,10 +1,16 @@
 # huggingface
 export HF_TOKEN="YOUR_HUGGINGFACE_TOKEN"
 
+# transformers
+export TOKENIZERS_PARALLELISM=false
+
 # accelerate config
 export ACCELERATE_DISTRIBUTED_TYPE="MULTI_GPU"
 export ACCELERATE_USE_MPS_DEVICE=0
 export ACCELERATE_USE_CPU=0
-export ACCELERATE_MIXED_PRECISION="no"
+export ACCELERATE_MIXED_PRECISION="yes"
 export ACCELERATE_GPU_IDS="all"
 export ACCELERATE_MULTI_GPU_BACKEND="gloo"
+
+
+# wandb disabled
