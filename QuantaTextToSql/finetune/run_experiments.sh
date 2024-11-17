d09a869794b6e4c77210f1eb3a4ec79849f94f37
Dhruv <dhruv@gretel.ai>
2024-11-17 07:17:41 +0000

    modify for experiments

--- QuantaTextToSql/finetune/run_experiments.sh
+++ QuantaTextToSql/finetune/run_experiments.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if logged in to Hugging Face
 if huggingface-cli whoami > /dev/null 2>&1; then
     echo "You are already logged in to Hugging Face."
 else
@@ -7,9 +8,16 @@ else
     huggingface-cli login
 fi
 
+# Define the project name for W&B
 project_name="sft_sql_interp"
-dataset_names=("withmartian/cs1_dataset" "withmartian/cs2_dataset" "withmartian/cs3_dataset")
-num_train_epochs=3
+
+# Define the list of datasets to train on
+dataset_names=(
+    "withmartian/cs1_dataset" 
+    "withmartian/cs2_dataset" 
+    "withmartian/cs3_dataset"
+)
+num_train_epochs=1
 batch_size=8
 gradient_accumulation_steps=8
 warmup_steps=100
@@ -18,12 +26,12 @@ warmup_steps=100
 export HF_TOKEN="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"
 
 # accelerate config
-export ACCELERATE_DISTRIBUTED_TYPE="MULTI_GPU"
-export ACCELERATE_USE_MPS_DEVICE=0
-export ACCELERATE_USE_CPU=0
-export ACCELERATE_MIXED_PRECISION="no"
-export ACCELERATE_GPU_IDS="all"
-export ACCELERATE_MULTI_GPU_BACKEND="gloo"
+#export ACCELERATE_DISTRIBUTED_TYPE="MULTI_GPU"
+#export ACCELERATE_USE_MPS_DEVICE=0
+#export ACCELERATE_USE_CPU=0
+#export ACCELERATE_MIXED_PRECISION="no"
+#export ACCELERATE_GPU_IDS="all"
+#export ACCELERATE_MULTI_GPU_BACKEND="gloo"
 
 sanitized_project_name=$(echo "$project_name" | tr '/\\:#?%,' '_')
 
@@ -34,16 +42,17 @@ max_seq_length=512
 
 # Define model and simplified model name for the experiment
 declare -A model_names=(
-    ["roneneldan/TinyStories-Instruct-2Layers-33M"]="TinyStories-2Layers-33M"
     ["meta-llama/Llama-3.2-1B-Instruct"]="Llama3.2-1B"
     ["Qwen/Qwen2.5-0.5B-Instruct"]="Qwen2.5-0.5B"
+    ["roneneldan/TinyStories-Instruct-2Layers-33M"]="TinyStories-2Layers-33M"
 )
 
 # Define the list of learning rates
-learning_rates=(1e-5 5e-5) # 1e-4 5e-4)
+learning_rates=(2e-5)
 
 experiment_counter=1
 
+# Loop through each model and dataset
 for model_name in "${!model_names[@]}"; do
     #simplified_model_name=$(echo "$model_name" | sed -E 's|.*/||; s/[-.]+/_/g')
     simplified_model_name="${model_names[$model_name]}"
@@ -54,10 +63,8 @@ for model_name in "${!model_names[@]}"; do
     sanitized_dataset_name=$(echo "$dataset_name" | tr '/\\:#?%,' '_')
 
     for dataset_name in "${dataset_names[@]}"; do
-        # Loop through each learning rate and run the script
-
-        # Start the sub-experiment counter for llama3.1 at 2.x
         sub_experiment_counter=1
+        # Loop through each learning rate and run the script
         for lr in "${learning_rates[@]}"; do
             # Create the experiment name like sft_llama3.1_lr_experiment_2.1, 2.2, etc.
             simplified_dataset_name="${dataset#*/}"
@@ -77,9 +84,9 @@ for model_name in "${!model_names[@]}"; do
             export TOKENIZERS_PARALLELISM=false
             
             # Run the Python script with the appropriate arguments
-            accelerate launch --num_processes 4 \
-            finetune_accelerate.py \
-                    --sft \
+            #accelerate launch --num_processes 4 \
+            #finetune_accelerate.py \
+            python finetune.py \
                     --model_name "$model_name" \
                     --learning_rate "$lr" \
                     --warmup_steps "$warmup_steps" \
