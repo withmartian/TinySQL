c3bbf4210a16dcd9b8a43aee3d336d7da3fb9cd9
PhilipQuirke <philipquirkenz@gmail.com>
2024-12-23 17:19:25 +1300

    attribution notebook v5

--- tests/test_corrupt_data.py
+++ tests/test_corrupt_data.py
@@ -5,10 +5,10 @@ from tests.test_util import TEST_DEVICE_MAP
 
 class TestCorruptData(unittest.TestCase):
 
-    def show_examples(self, feature_name, model_num, cs_num=1, use_corrupt_names=False):
+    def show_examples(self, feature_name, model_num, cs_num=1, use_novel_names=False):
         tokenizer, _ = load_sql_interp_model(model_num, cs_num, use_flash_attention=False, device_map=TEST_DEVICE_MAP)
          
-        generator = CorruptFeatureTestGenerator(model_num=model_num, cs_num=cs_num, tokenizer=tokenizer, use_corrupt_names=use_corrupt_names)
+        generator = CorruptFeatureTestGenerator(model_num=model_num, cs_num=cs_num, tokenizer=tokenizer, use_novel_names=use_novel_names)
 
         batch_size = 5
         examples = generator.generate_feature_examples(feature_name, batch_size)      
@@ -34,8 +34,10 @@ class TestCorruptData(unittest.TestCase):
             assert example.answer_token_index > 0
             assert example.prompt_token_index < example.answer_token_index 
 
-            clean_tokens = tokenizer(example.clean_BatchItem.get_alpaca_prompt() + example.clean_BatchItem.sql_statement)["input_ids"]
-            corrupt_tokens = tokenizer(example.corrupt_BatchItem.get_alpaca_prompt() + example.corrupt_BatchItem.sql_statement)["input_ids"]
+            clean_str = example.clean_BatchItem.get_alpaca_prompt() + example.clean_BatchItem.sql_statement
+            corrupt_str = example.corrupt_BatchItem.get_alpaca_prompt() + example.corrupt_BatchItem.sql_statement
+            clean_tokens = tokenizer(clean_str)["input_ids"]
+            corrupt_tokens = tokenizer(corrupt_str)["input_ids"]
 
             # All (clean or corrupt) examples should have the same prompt+answer length
             assert len(clean_tokens) == len_all_tokens
@@ -47,6 +49,8 @@ class TestCorruptData(unittest.TestCase):
             # PQR TODO This does not work for modelNum==2. Not sure why
             if model_num <= 1: 
                 if corrupt_tokens[example.prompt_token_index] != example.corrupt_tokenizer_index:
+                    print(clean_str.replace('\n', ' '))
+                    print(corrupt_str.replace('\n', ' '))
                     print( "Bad prompt corrupt token:", example.prompt_token_index, example.corrupt_tokenizer_index, corrupt_tokens[example.prompt_token_index], corrupt_tokens)
                     assert False
                 #if corrupt_tokens[example.answer_token_index] != example.corrupt_tokenizer_index:
@@ -61,49 +65,49 @@ class TestCorruptData(unittest.TestCase):
     def test_m1_generate_name_tokens(self): 
         tokenizer, _ = load_sql_interp_model(1, 2, use_flash_attention=False, device_map=TEST_DEVICE_MAP)
          
-        generator = CorruptFeatureTestGenerator(model_num=1, cs_num=2, tokenizer=tokenizer, use_corrupt_names=True)
+        generator = CorruptFeatureTestGenerator(model_num=1, cs_num=2, tokenizer=tokenizer, use_novel_names=True)
 
         for word in generator.clean_table_names:
             assert len(tokenizer(word)["input_ids"]) == 1
 
-        for word in generator.corrupt_table_names:
+        for word in generator.novel_table_names:
             assert len(tokenizer(word)["input_ids"]) == 1
 
         for word in generator.clean_field_names:
             assert len(tokenizer(word)["input_ids"]) == 1
 
-        for word in generator.corrupt_field_names:
+        for word in generator.novel_field_names:
             assert len(tokenizer(word)["input_ids"]) == 1
 
         for word in generator.clean_field_types:
             assert len(tokenizer(word)["input_ids"]) == 1
 
     def test_m1_generate_ENGTABLENAME(self): 
-        self.show_examples(ENGTABLENAME, 1, use_corrupt_names=False)
-        self.show_examples(ENGTABLENAME, 1, use_corrupt_names=True)
+        self.show_examples(ENGTABLENAME, 1, use_novel_names=False)
+        self.show_examples(ENGTABLENAME, 1, use_novel_names=True)
 
 
     def test_m1_generate_ENGFIELDNAME(self):   
-        self.show_examples(ENGFIELDNAME, 1, use_corrupt_names=False)
-        self.show_examples(ENGFIELDNAME, 1, use_corrupt_names=True)
+        self.show_examples(ENGFIELDNAME, 1, use_novel_names=False)
+        self.show_examples(ENGFIELDNAME, 1, use_novel_names=True)
 
     # Suppress until CREATE is in the TinyStories Vocab     
     #def test_generate_DEFCREATETABLE(self):    
     #    self.show_examples(DEFCREATETABLE, 1)
 
     def test_m1_generate_DEFTABLENAME(self):   
-        self.show_examples(DEFTABLENAME, 1, use_corrupt_names=False)
-        self.show_examples(DEFTABLENAME, 1, use_corrupt_names=True)
+        self.show_examples(DEFTABLENAME, 1, use_novel_names=False)
+        self.show_examples(DEFTABLENAME, 1, use_novel_names=True)
 
     # Need to debug how "," is tokenized 
     #def test_generate_DEFFIELDSEPARATOR(self):   
     #    self.show_examples(DEFFIELDSEPARATOR, 1)
 
     def test_m1_generate_DEFFIELDNAME(self):  
-        self.show_examples(DEFFIELDNAME, 1, use_corrupt_names=False)
-        self.show_examples(DEFFIELDNAME, 1, use_corrupt_names=True)
+        self.show_examples(DEFFIELDNAME, 1, use_novel_names=False)
+        self.show_examples(DEFFIELDNAME, 1, use_novel_names=True)
 
     def test_m2_generate_DEFFIELDNAME(self):  
-        self.show_examples(DEFFIELDNAME, 2, use_corrupt_names=False)
-        self.show_examples(DEFFIELDNAME, 2, use_corrupt_names=True)
+        self.show_examples(DEFFIELDNAME, 2, use_novel_names=False)
+        self.show_examples(DEFFIELDNAME, 2, use_novel_names=True)
 
